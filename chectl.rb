e54036112c0cbe17da20d50f31d08a4efb5780f0
Adam Karsenti <adamkarsenti@MacBook-Pro-de-Adam.local>
2019-07-12 17:13:16 +0200

    install pkg

--- chectl.rb
+++ chectl.rb
@@ -6,21 +6,22 @@ class Chectl < Formula
   homepage ""
   url "https://github.com/che-incubator/chectl/releases/download/20190701164846/chectl-v0.0.2-6c96c58-darwin-x64.tar.gz"
   sha256 "27e53835c1ee31056411aeea39f68d5afe59cd9fe7a5fb856732d1217d130b26"
-  # depends_on "cmake" => :build
+  depends_on "yarn" => :build
+  depends_on "p7zip" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    system "make", "install" # if this fails, try separate make/make install steps
+
+    system "npm", "install", "-g", "pkg@4.3.8"
+
+
+
+
+
+
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
+    # `test do` will create, run in and delete a temporary directory
     #
     # This test will fail and we won't accept that! For Homebrew/homebrew-core
     # this will need to be a test that verifies the functionality of the
